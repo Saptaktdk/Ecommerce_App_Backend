8c27e53f2252828bb716fda6d9ad7e6d82028885
Saptaktdk <saptakdev123@gmail.com>
2025-10-13 19:19:05 +0000

    added requestmapping annotaion

--- ecommerce/src/main/java/com/app/ecommerce/UserController.java
+++ ecommerce/src/main/java/com/app/ecommerce/UserController.java
@@ -9,19 +9,22 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
 
-    @GetMapping("/api/users")
+    @GetMapping
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<List<User>> getAllUsers() {
 
         return ResponseEntity.ok(userService.fetchAllUsers());
         //return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("/{id}")
+    //@RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
 
         return userService.fetchUser(id)
@@ -29,13 +32,13 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok("User added successfully");
     }
 
-    @PutMapping("/api/users/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@PathVariable("id") Long id, @RequestBody User updatedUser) {
         boolean updated = userService.updateUser(id, updatedUser);
         if (updated) {
