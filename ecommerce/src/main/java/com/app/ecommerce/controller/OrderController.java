ce6ba245948df3243bb7731c1db2c1852768d9bc
Saptaktdk <saptakdev123@gmail.com>
2025-11-13 17:27:50 +0000

    Implementing place order functionlity

---
+++ ecommerce/src/main/java/com/app/ecommerce/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.app.ecommerce.controller;
+
+import com.app.ecommerce.dto.OrderResponse;
+import com.app.ecommerce.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(
+            @RequestHeader("X-User-ID") String userId) {
+        OrderResponse order = orderService.createOrder(userId);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+
+    }
+
+
+}
